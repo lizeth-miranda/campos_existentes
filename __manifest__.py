2dda68dacb251f0cf124be25f14cb8779a070221
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2021-01-26 13:23:01 -0700

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Campos Extras',
+    'version': '13.1',
+    'author': 'Demsa',
+    'website': '',
+    'depends': [
+        'purchase',
+    ],
+    'data': [
+        # security
+        # data
+        # demo
+        # reports
+        # views
+        'views/purchase_order.xml',
+        'views/stock_picking.xml',
+        'views/account_move.xml',
+        'views/hr_employee.xml',
+
+
+    ],
+}
