9d8453c93671760f95e9ed86de30a1d03478e7ec
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2022-02-17 13:29:22 -0700

    Add files via upload

---
+++ models/purchase_requisition.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# instruccion para hacer importaciones desde odoo
+from odoo import fields, models
+
+
+class acuerdo_compra(models.Model):
+    _inherit = "purchase.requisition"
+
+    state = fields.Selection(selection_add=[(
+        "Autorizar", "Autorizado"), ("ongoing",)])
+    state_blanket_order = fields.Selection(selection_add=[(
+        "Autorizar", "Autorizado"), ("ongoing",)])
+
+    def action_autorizar(self):
+        self.write({'state': 'Autorizar'})
+
+    #doc_ori = fields.Many2one('Documento Origen Prueba', 'documento.origen',)
+    notas = fields.Text(string="Notas",)
