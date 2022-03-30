a6587f7b717a3061c6b0c3ab9226ebbb632720bf
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2022-03-29 17:02:55 -0700

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,6 @@
 #from . import purchase_order
 #from . import stock_picking
 from . import account_move
-from . import purchase_requisition
+#from . import purchase_requisition
 from . import sale_order
 #from . import hr_employee
