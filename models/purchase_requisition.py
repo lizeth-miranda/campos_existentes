973272c8789f4459bc64646a764ffb0efc39694a
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2022-03-29 17:24:03 -0700

    Update purchase_requisition.py

--- models/purchase_requisition.py
+++ models/purchase_requisition.py
@@ -6,13 +6,15 @@ from odoo import fields, models
 class acuerdo_compra(models.Model):
     _inherit = "purchase.requisition"
 
-    state = fields.Selection(selection_add=[(
-        "Autorizar", "Autorizado"), ("ongoing",)])
-    state_blanket_order = fields.Selection(selection_add=[(
-        "Autorizar", "Autorizado"), ("ongoing",)])
-
-    def action_autorizar(self):
-        self.write({'state': 'Autorizar'})
+     # state = fields.Selection(selection_add=[(
+    #     "Autorizar", "Autorizado"), ("ongoing",)])
+    # state_blanket_order = fields.Selection(selection_add=[(
+    #     "Autorizar", "Autorizado"), ("ongoing",)])
+    usuario = fields.Many2one(
+        'res.users', string='Usuario',
+        default=lambda self: self.env.user)
+    # def action_autorizar(self):
+    #     self.write({'state': 'Autorizar'})
 
     #doc_ori = fields.Many2one('Documento Origen Prueba', 'documento.origen',)
     #notas = fields.Text(string="Notas",)
