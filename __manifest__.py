2ebf15bbb86e5bc41213a2cebc3172e25d8b4528
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2022-09-29 11:24:27 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Campos Extras',
-    'version': '13.4',
+    'version': '13.5',
     'author': 'Demsa',
     'website': '',
     'depends': ['base', 'purchase_requisition', 'account'],
